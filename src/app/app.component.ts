f05074fd3ac12b724515492658bc4845e90715c3
Martinez24 <ponchomarpez@gmail.com>
2018-09-25 13:06:08 -0500

    Agregando paginas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,37 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { TabsPage } from "../pages/tabs/tabs";
+import { NosotrosPage } from "../pages/nosotros/nosotros";
+import { CartaPage } from "../pages/carta/carta";
+import { Reservacion_1Page } from "../pages/reservacion-1/reservacion-1";
+import { PerfilPage } from "../pages/perfil/perfil";
+import { HistorialPage } from "../pages/historial/historial";
+
+
+
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = LoginPage;
+  home = TabsPage;
+  nosotros = NosotrosPage;
+  carta = CartaPage;
+  perfil = PerfilPage;
+  historial = HistorialPage;
+  reservacion = Reservacion_1Page;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+
+  constructor(platform: Platform,
+     statusBar: StatusBar,
+      splashScreen: SplashScreen,
+    public menuCtrl: MenuController) {
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -18,5 +39,45 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+//Menu de la aplicacion
+  irHome( home:any ){
+  console.log( home );
+  this.rootPage=home;
+  this.menuCtrl.close();
+}
+  irNosotros( nosotros:any ){
+  console.log( nosotros );
+  this.rootPage= nosotros;
+  this.menuCtrl.close();
 }
 
+  irPerfil( perfil:any ){
+  console.log( perfil );
+  this.rootPage= perfil;
+  this.menuCtrl.close();
+  }
+
+  irCarta( carta:any ){
+  console.log( carta );
+  this.rootPage= carta;
+  this.menuCtrl.close();
+  }
+
+  irHistorial( historial:any ){
+  console.log( historial );
+  this.rootPage= historial;
+  this.menuCtrl.close();
+  }
+
+  irReservacion( reservacion:any ){
+  console.log( reservacion );
+  this.rootPage= reservacion;
+  this.menuCtrl.close();
+  }
+
+  irLogin( rootPage ){
+  console.log( rootPage );
+  this.rootPage= rootPage;
+  this.menuCtrl.close();
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,80 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { PerfilPage } from "../pages/perfil/perfil";
+import { EventosPage } from "../pages/eventos/eventos";
+import { NosotrosPage } from "../pages/nosotros/nosotros";
+import { CartaPage } from "../pages/carta/carta";
+import { HistorialPage } from "../pages/historial/historial";
+import { Reservacion_1Page } from "../pages/reservacion-1/reservacion-1";
+import { TabsPage } from "../pages/tabs/tabs";
+
+
+//import { AuthService } from '../providers/auth-service';
+//Firebase
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+//providers
+import { UsuarioProvider } from '../providers/usuario/usuario';
+
+//Plugins
+import { Facebook } from '@ionic-native/facebook';
+import { GooglePlus } from '@ionic-native/google-plus';
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBixlCb21nNbPSurY-Pvqu3hZB80Icl9Pk",
+  authDomain: "guestreservation-8b24b.firebaseapp.com",
+  databaseURL: "https://guestreservation-8b24b.firebaseio.com",
+  projectId: "guestreservation-8b24b",
+  storageBucket: "guestreservation-8b24b.appspot.com",
+  messagingSenderId: "853477386824"
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    PerfilPage,
+    TabsPage,
+    EventosPage,
+    Reservacion_1Page,
+    NosotrosPage,
+    CartaPage,
+    HistorialPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    PerfilPage,
+    TabsPage,
+    EventosPage,
+    Reservacion_1Page,
+    NosotrosPage,
+    CartaPage,
+    HistorialPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    AngularFireDatabase,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    UsuarioProvider,
+    Facebook,
+    GooglePlus
+    //AuthService
   ]
 })
 export class AppModule {}
